78bf1c37ad7ba81131d210028a45617c76a35752
lixiong <qqlixiongjob@163.com>
2021-06-18 13:30:19 +0800

    ...

--- app/src/main/java/com/lx/arcprogress/MainActivity.java
+++ app/src/main/java/com/lx/arcprogress/MainActivity.java
@@ -1,11 +1,10 @@
 package com.lx.arcprogress;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
