748e82bdfa1aee1bbc0913b19c8673575acfd374
lixiong <qqlixiongjob@163.com>
2024-01-29 20:36:37 +0800

    ...

--- app/src/main/java/com/lx/arcprogress/MainActivity.java
+++ app/src/main/java/com/lx/arcprogress/MainActivity.java
@@ -3,11 +3,14 @@ package com.lx.arcprogress;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.ved.framework.utils.Utils;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Utils.init(this.getApplication());
     }
 }
